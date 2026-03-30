b2fdeceee06cfba37fc89cd870857bbabe909de4
Vitor Carvalhal <devcarvalhal@gmail.com>
2026-03-29 21:42:03 -0300

    feat: modificate base customers and transaction tables

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,3 +1,20 @@
-create table customers (
-	customer_id int primary key
+CREAT TABLE customers (
+	customer_id INT PRIMARY KEY,
+	costumer_name VARCHAR(100),
+	account_age_days INT,
+	customer_segment VARCHAR(50),
+	home_city VARCHAR(50),
+	home_country VARCHAR(50)
+);
+
+CREAT TABLE transactions (
+	transaction_id INT PRIMARY KEY,
+	customer_id INT,
+	amount NUMERIC,
+	status VARCHAR(20),
+	channel VARCHAR(20),
+	city VARCHAR(50),
+	country VARCHAR(50),
+	is_international BOOLEAN,
+	risk_signal VARCHAR(50)
 );
